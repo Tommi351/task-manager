f9a21cb6e8cdeeeb667fa5b786b01dafbabbe8d6
Tomi O <to.olufemi1@gmail.com>
2025-10-24 19:38:03 -0600

    Setting backend up for deployment

--- backend/server.js
+++ backend/server.js
@@ -27,18 +27,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/tasks', taskRoutes);
 app.use('/users', userRoutes);
 
-// Serve frontend
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/dist')));
-
-  app.get(/^(?!\/api).*/, (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, '../', 'frontend', 'dist', 'index.html')
-    )
-  );
-} else {
-  app.get('/', (req, res) => res.send('Please set to production'));
-}
 
 app.listen(PORT, () => {
    console.log(`App is listening on Port ${PORT}`)
